8e0db3b6ad05a52ebaa6e1454229b3a657d2b79d
Rafael Chavez <rafaelchavezsolis@gmail.com>
2023-02-26 22:34:11 -0600

    Add simple calculator logic

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-#![allow(unused)]
-
 use std::io;
-use rand::Rng;
-use std::io::{Write, BufRead, BufReader, ErrorKind};
-use std::fs::File;
-use std::cmp::Ordering;
-
+use std::io::Write;
+use std::env::{args, Args};
 
 fn main() {
+    let mut args: Args = args();
     print!("Welcome, what is your name? ");
-    io::stdout().flush();
+    io::stdout().flush().expect("Failed to flush");
     // By default all variables are immutable
     let greeting = "Hello";
     // To make a variable mutable, use the mut keyword
@@ -18,13 +14,40 @@ fn main() {
     io::stdin().read_line(&mut name).expect("Failed to read line");
 
     println!("{} {}", greeting, name.trim_end() );
-    println!("Waht is your age?");
-    // Constants are always immutable
-    const ONE_MILL: u32 = 1_000_000;
+    println!("How old are you?");
     // Shadowing is a way to change the type of a variable by using the same name
     let mut age = String::new();
     io::stdin().read_line(&mut age).expect("Failed to read line");
     let age: u32 = age.trim_end().parse().expect("Please type a number!");
     println!("You are {} years old", age);
+    println!("Welcome to the calculator");
+    // Get console arguments
+    if args.len() < 3 {
+        println!("Please provide 2 numbers and an operator");
+        return;
+    }
+    let first_element = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
+    let second_element = args.nth(0).unwrap();
+    println!("Operation: {} {} {}", first_element, operator, second_element);
+    let first_element:f32 = first_element.parse::<f32>().unwrap();
+    let second_element:f32 = second_element.parse::<f32>().unwrap();
+
+    let result = make_operation(operator, first_element, second_element);
+    println!("{}", output_result(operator, first_element, second_element, result));
+}
+
+fn make_operation(operator: char, first_number:f32, second_number:f32) -> f32{
+    match operator {
+        '+' => first_number + second_number,
+        '-' => first_number - second_number,
+        '*' => first_number * second_number,
+        '/' => first_number / second_number,
+        _ => 0.0
+        
+    }
+}
+fn output_result(operator: char, first_number:f32, second_number:f32, result: f32) -> String {
+    format!("{} {} {} = {}", first_number, operator, second_number, result)
 }
 // Path: src/main.rs
